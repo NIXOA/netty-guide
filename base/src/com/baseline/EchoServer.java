3d96c4cde8af6ca99d40ec0efaf8bbca8db80420
Goslar <qxwandota@163.com>
2019-09-10 23:39:44 +0800

    添加服务端方法

--- base/src/com/baseline/EchoServer.java
+++ base/src/com/baseline/EchoServer.java
@@ -11,6 +11,7 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import java.net.InetSocketAddress;
 
 /**
+ * netty服务端
  * @Author Godzilla
  * @Date 2019/9/9 23:26
  * @Version 1.0
@@ -19,33 +20,45 @@ public class EchoServer {
 
     private final int port;
 
-    public EchoServer(int port) {
-        this.port = port ;
+    private EchoServer(int port) {
+        this.port = port;
     }
 
     public static void main(String[] args) throws InterruptedException {
-        if (args.length!=1){
+        if (args.length != 1) {
             System.out.println("Usage: " + EchoServer.class.getSimpleName() + " <port>");
         }
 
-        int port=Integer.parseInt(args[0]);
+        int port = Integer.parseInt(args[0]);
 
         new EchoServer(port).start();
 
 
     }
 
-    public void start() throws InterruptedException {
-        final EchoServerHandle serverHandle=new EchoServerHandle();
-        EventLoopGroup group=new NioEventLoopGroup();
+
+    /**
+     * 创建netty服务过程:
+     *
+     * -> 创建一个ServerBootstrap的实例已引导和绑定服务器
+     * -> 创建并且分配一个NioEventLoopGroup实例进行事件的处理
+     * -> 指定服务器绑定的本地InetSocketAddress
+     * -> 使用一个handler的实例初始化每一个新的channel
+     * -> 调用ServerBootstrap.bin()方法已绑定服务器
+     *
+     * @throws InterruptedException
+     */
+    private void start() throws InterruptedException {
+        final EchoServerHandle serverHandle = new EchoServerHandle();
+        EventLoopGroup group = new NioEventLoopGroup();
         try {
-            ServerBootstrap bootstrap=new ServerBootstrap();
+            ServerBootstrap bootstrap = new ServerBootstrap();
             bootstrap.group(group)
                     //指定所使用NIO传输的Channel
                     .channel(NioServerSocketChannel.class)
                     //指定接口的套接字地址
                     .localAddress(new InetSocketAddress(port))
-                    //添加自定义的handle
+                    //添加自定义的handle,channelInitializer负责将handler添加到ChannelPipeLine中
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
@@ -53,7 +66,7 @@ public class EchoServer {
                         }
                     });
             //异步绑定服务器，调用sync()方法阻塞直到绑定完成
-            ChannelFuture future=bootstrap.bind().sync();
+            ChannelFuture future = bootstrap.bind().sync();
             //获取Channel的CloseFuture,并且阻塞当前线程直它完成
             future.channel().closeFuture().sync();
         } catch (Exception e) {
